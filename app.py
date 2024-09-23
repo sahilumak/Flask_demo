8402a10578e20f8e55de9970bba0828a5061b5c7
“[firstname <sahilumak8@gmail.com>
2024-09-23 20:56:27 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-print("hello world")
-git config --global user.name “[firstname lastname]”
+def armstrong(num):
+    result = 0
+    acc = num
+    while num:
+        a = num%10
+        result = result * 10 + a
+        num = num//10
+
+    if result==acc:
+        return 1
+    else:
+        return 0
